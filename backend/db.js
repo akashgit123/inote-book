4e2bc627daf7d9f842c7103d1dc67576f61be699
Akash <akashshettigar709@gmail.com>
2023-09-18 10:10:37 +0530

    db and index setup

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const connectToDb = async() =>{
+    mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(() => console.log('Connected!'));
+}
+
+module.exports = connectToDb;
