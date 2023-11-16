66ebe4168dcd93af2ccaf81df197f1a94694a82d
Akash <akashshettigar709@gmail.com>
2023-11-16 18:57:55 +0530

    Adding login

---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+import React,{useState} from "react";
+import {useNavigate} from 'react-router-dom';
+
+export default function Login() {
+    const [credentials,setCredentials] = useState({email:" ",password:""});
+    const host = "http://localhost:7000";
+    let navigate = useNavigate();
+    
+    const handleOnSubmit = async(e) =>{
+        e.preventDefault();
+        const response = await fetch(`${host}/api/auth/login`,{
+            method:"POST",
+            headers :{
+                'Content-Type':'application/json',
+            },
+            body : JSON.stringify({email:credentials.email,password:credentials.password})
+        });
+        const token = await response.json();
+        console.log(token)
+        if(token){
+            //set the auth token
+            localStorage.setItem('token',token);
+            navigate('/');
+        }
+        else{
+            alert("Wrong credentials");
+        }
+    }
+
+    const handleOnChange =(e)=>{
+        setCredentials({...credentials,[e.target.name]:e.target.value});
+    }
+
+  return (
+    <div className="container">
+      <form onSubmit={handleOnSubmit} >
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            value={credentials.email}
+            onChange={handleOnChange}
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            onChange={handleOnChange}
+            value={credentials.password}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,15 +39,17 @@ export default function Navbar() {
               </li>
             </ul>
             <form className="d-flex">
-              <input
+              {/* <input
                 className="form-control me-2"
                 type="search"
                 placeholder="Search"
                 aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
+              /> */}
+              {/* <button className="btn btn-outline-success" type="submit">
                 Search
-              </button>
+              </button> */}
+              <Link to="/login" className="btn btn-primary mx-1" role="button">Login</Link>
+              <Link to="/signup" className="btn btn-primary mx-1" role="button">Signup</Link>
             </form>
           </div>
         </div>
