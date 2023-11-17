3ca465b832cbe7e5b31357cf260999ff122c6a61
Akash <akashshettigar709@gmail.com>
2023-11-17 16:27:42 +0530

    About content

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
-import { useEffect, useContext } from "react";
-
-const About = () =>{
+const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="container mx-10">
+      <div className="card text-center">
+        <div className="card-header">*************</div>
+        <div className="card-body">
+          <h5 className="card-title">About Us - Inote book</h5>
+          <pre className="card-text">
+          INotebook is a React Application for managing personal notes on the cloud.
+          </pre>
+        </div>
+        <div className="card-footer text-muted m5">*************</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About ;
+export default About;
